49143ecdaf08e673dab972c17055bdfce17c6cf3
salpanis <salpanis@gmail.com>
2022-06-24 17:02:43 +0100

    Get single trip route added

--- backend/leo-kos-backend/app/core/routers/trips.py
+++ backend/leo-kos-backend/app/core/routers/trips.py
@@ -15,9 +15,16 @@ async def read_trips():
     trips = await db['trips'].find().to_list(1000)
     return trips
 
+@router.get('/{id}', response_description='Get a single trip', response_model=TripModel)
+async def show_trip(id: str):
+    if (trip := await db['trips'].find_one({"_id": id})) is not None:
+        return trip
+    
+    raise HTTPException(status_code=404, detail=f'Trip {id} not found')
+
 @router.post('/', response_description='Add new trip', response_model=TripModel)
 async def create_trip(trip: TripModel = Body(...)):
     trip = jsonable_encoder(trip)
-    new_trip = await db['trips_trip'].insert_one(trip)
-    created_trip = await db['trips_trip'].find_one({"_id": new_trip.inserted_id})
+    new_trip = await db['trips'].insert_one(trip)
+    created_trip = await db['trips'].find_one({"_id": new_trip.inserted_id})
     return JSONResponse(status_code = status.HTTP_201_CREATED, content=created_trip)
