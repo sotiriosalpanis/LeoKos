3f94650a67cbb12f5abddd1ca01b475e33ab36ce
salpanis <salpanis@gmail.com>
2022-06-28 17:13:20 +0100

    Added more detail to trip/[id] page

---
+++ leo-kos-app/pages/trips/[id].js
@@ -0,0 +1,51 @@
+
+
+const tripsURL = `${process.env.DETA_ENDPOINT}trips`
+const apiKey = process.env.DETA_API_KEY
+
+export async function getStaticProps( {params}) {
+  const {id} = params
+  const res = await fetch(`${tripsURL}/${id}`, {
+    headers : {
+      'X-API-Key': apiKey
+    }
+  })
+  const trip = await res.json()
+    return {props: {
+      trip,
+    }}
+  }
+
+export async function getStaticPaths() {
+  const res = await fetch(tripsURL, {
+    headers : {
+      'X-API-Key': apiKey
+    }
+  })
+  const trips = await res.json()
+  const paths = trips.map((trip) => ({
+    params: { id: trip._id }
+  }))
+
+  return { paths, fallback: false}
+}
+
+export default function TripLanding({ trip }) {
+
+  return (
+    <div>
+      <h2>{trip.trip_name}</h2>
+      <div>
+        {trip.description}
+      </div>
+      <div>
+        {trip.stops.map((stop, index) => {
+          return <h3 key={index}>
+            {stop.stop_name}
+          </h3>
+        })}
+      </div>
+    </div>
+    
+  )
+}
